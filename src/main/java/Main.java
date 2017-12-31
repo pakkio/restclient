5fcf53ca55706c80e615467a55de7cc4140e6777
Salahzar Stenvaag <salahzar@gmail.com>
2017-12-31 02:02:41 +0100

    further tests show that jackson is not working with some json embedding newlines so we added gson instead.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,24 @@
+import com.google.gson.Gson;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class Main {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
-        Ip got = restTemplate.getForObject("http://ip.jsontest.com/", Ip.class);
-
+        ResponseEntity<Ip> got = restTemplate.getForEntity("http://ip.jsontest.com/", Ip.class);
+        ResponseEntity<String> raw = restTemplate.getForEntity("http://ip.jsontest.com/", String.class);
         System.out.println(got);
+        System.out.println(raw);
+
+        Gson gson = new Gson();
+        Origin gotJackson = restTemplate.getForObject("http://httpbin.org/ip", Origin.class);
+        ResponseEntity<String> raw2 = restTemplate.getForEntity("http://httpbin.org/ip", String.class);
+        Origin gotGson = gson.fromJson(raw2.getBody(), Origin.class);
+
+        // this testify that gson works better handling embedded newlines
+        System.out.println("Jackson: " + gotJackson);
+        System.out.println("Json: " + gotGson);
+        System.out.println(raw2);
+
     }
 }
